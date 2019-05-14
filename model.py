56969a1867ee2c5a9a5003a7c14a2553d5858bd7
Jan Jakončič <jan.jakoncic@student.fmf.uni-lj.si>
2019-05-14 16:40:08 +0200

    dokoncanje

--- model.py
+++ model.py
@@ -25,12 +25,40 @@ class Igra:
 	
 	def poraz(self):
 		return self.stevilo_napak() >= STEVILO_DOVOLJENIH_NAPAK
+	
+	def pravilni_del_gesla(self):
+		novi = ''
+		for c in self.geslo:
+			if c in self.crke:
+				novi += c
+			else:
+				novi += '_'
+		return novi
+		
+	def nepravilni_ugibi(self):
+		return " ".join(c for c in self.crke if c not in self.geslo)
+	
+	def ugibaj(self, crka):
+		crka = crka.upper()
+		if crka in self.crke:
+			return PONOVLJENA_CRKA
+		if crka in self.geslo():
+			self.crke.append(crka)
+			if self.zmaga():
+				return ZMAGA
+			return PRAVILNA_CRKA
+		# potem je napačna neponovljena
+		self.crke.append(crka)
+		if self.poraz():
+			return PORAZ
 		
 ## TESTNI PROGRAM ##
 igra = Igra("nekaj")
-igra.crke = ['a','l','v','n','x','e','k','j']
+igra.crke = ['a','l','v','n','x']
 print(igra.napacne_crke())
 print(igra.pravilne_crke())
 print(igra.stevilo_napak())
 print(igra.zmaga()) 
 print(igra.poraz())
+print(igra.pravilni_del_gesla())
+print(igra.nepravilni_ugibi())
